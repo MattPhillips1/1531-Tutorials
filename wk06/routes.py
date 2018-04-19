25b2e5072bb98f05254c3d5a743c5ad6a9a16eff
MattPhillips1 <matt.phillips121@gmail.com>
2018-04-19 10:54:42 +1000

    Add files via upload

---
+++ wk06/routes.py
@@ -0,0 +1,15 @@
+from flask import Flask, redirect, render_template, request, url_for
+from server import app
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form['name']
+        zID = request.form['zID']
+        desc = request.form['desc']
+        return redirect(url_for('hello', name=name, id=zID, desc=desc))
+    return render_template('index.html')
+
+@app.route("/hello/<name>/<id>/<desc>")
+def hello(name, id, desc):
+    return render_template('hello.html', name=name, id=id, desc=desc)
