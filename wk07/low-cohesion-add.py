943ab4672267e067a3211cb0993064deeb7c89e3
Matthew <matt.phillips121@gmail.com>
2018-04-19 11:31:00 +1000

    Upload the examples

---
+++ wk07/low-cohesion-add.py
@@ -0,0 +1,7 @@
+def add():
+    num_1 = int(input("Enter a number: "))
+    num_2 = int(input("Enter a number: "))
+    sum = num_1 + num_2
+    return sum
+
+print("The sum of the numbers is: %d" % add())
---
+++ wk07/low-cohesion-exp.py
@@ -0,0 +1,7 @@
+def exponent():
+    num = int(input("Enter in a number: "))
+    exponent = int(input("Raise the number to the power of: "))
+    answer = pow(num, exponent)
+    print(answer)
+
+exponent()
